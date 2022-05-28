c65542415ff5932a843064c67c5b551be8e629cf
Natdrags <98502921+Natdrags@users.noreply.github.com>
2022-05-28 18:41:26 -0500

    retrofit

---
+++ app/src/main/java/Christopher/Martinez/retrofitget/DogViewHolder.kt
@@ -0,0 +1,13 @@
+package Christopher.Martinez.retrofitget
+
+import Christopher.Martinez.retrofitget.databinding.ItemDogBinding
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class DogViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    private val binding = ItemDogBinding.bind(view)
+    fun bind(image:String){
+        Picasso.get().load(image).into(binding.ivDog)
+    }
+}
